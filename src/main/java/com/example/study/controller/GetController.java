de16abd9790b10f096ca1c81afea1198a5c701eb
dabok407 <dabok407@naver.com>
2019-11-14 23:18:08 +0900

    HTTP Method

--- src/main/java/com/example/study/controller/GetController.java
+++ src/main/java/com/example/study/controller/GetController.java
@@ -1,16 +1,35 @@
 package com.example.study.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.study.model.SearchParam;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 public class GetController {
 
-    @RequestMapping(method = RequestMethod.GET, path = "/test")
+    @RequestMapping(method = RequestMethod.GET, path = "/getMethod")
     public String getRequest(){
 
         return "Hi getMethod2";
     }
+
+    @GetMapping("/getParameter")
+    public String getParameter(@RequestParam String id, @RequestParam(name = "password") String pwd){
+        System.out.println("id : " + id);
+        System.out.println("password : " + pwd);
+        return id + "//" + pwd;
+    }
+
+    // SpringBoot에서는 jackson 라이브러리를 내장.
+    // 객체를 반환 시 json형태로 반환됨.
+    @GetMapping("/getMultiParameter")
+    public SearchParam getMultiParameter(SearchParam searchParam){
+
+        System.out.println(searchParam.getAccount());
+        System.out.println(searchParam.getEmail());
+        System.out.println(searchParam.getPage());
+
+        return searchParam;
+    }
+
 }
---
+++ src/main/java/com/example/study/model/SearchParam.java
@@ -0,0 +1,13 @@
+package com.example.study.model;
+
+import lombok.Data;
+
+@Data
+public class SearchParam {
+
+    public String account;
+    private String email;
+    private int page;
+
+
+}
