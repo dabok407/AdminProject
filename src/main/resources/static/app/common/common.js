b85d9585b729dd176ece5483ee9492ce8b98ac87
dabok407 <dabok407@naver.com>
2019-12-22 23:14:59 +0900

    ajax호출 공통 함수 추가

--- src/main/resources/static/app/common/common.js
+++ src/main/resources/static/app/common/common.js
@@ -9,3 +9,25 @@ common.getContextPath = function getContextPath() {
 common.href = function href(url){
     location.href = common.getContextPath()+"/"+url;
 }
+
+common.serializeObject = function(formId) {
+    var returnObj = null;
+    var data = null;
+    var $selector = $('#'+formId);
+    try {
+        if($selector[0].tagName && $selector[0].tagName.toUpperCase() == "FORM" ) {
+            var arr = $selector.serializeArray();
+            if(arr){
+                returnObj = {};
+                data = {};
+                jQuery.each(arr, function() {
+                    data[this.name] = this.value;
+                });
+            }
+        }
+    }catch(e) {
+        alert(e.message);
+    }finally {}
+    returnObj["data"] = data;
+    return returnObj;
+}
